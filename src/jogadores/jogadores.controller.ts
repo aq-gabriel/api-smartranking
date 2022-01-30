7ecacfb1f9401783493c58bce7aeb5de40ff30fc
Gabriel Aquino <40728047+aq-gabriel@users.noreply.github.com>
2022-01-30 18:29:13 -0300

    feat: implements UsePipes and ValidationPipe

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,5 +1,14 @@
 import { JogadoresService } from './jogadores.service';
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 
@@ -8,6 +17,7 @@ export class JogadoresController {
   constructor(private readonly jogadoresService: JogadoresService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     return await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
   }
