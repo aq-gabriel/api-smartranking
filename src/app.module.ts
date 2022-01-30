704dc0d69885538794925d5688894baa1217f93b
Gabriel Aquino <40728047+aq-gabriel@users.noreply.github.com>
2022-01-30 16:32:12 -0300

    feat: adding mongoose module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:nicolas091020@cluster0.k9nfz.mongodb.net/smartranking?retryWrites=true&w=majority',
+    ),
+    JogadoresModule,
+  ],
 })
 export class AppModule {}
