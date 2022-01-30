64378ba55925d4ef8a585febb32d8bb7e4d5ce65
Gabriel Aquino <40728047+aq-gabriel@users.noreply.github.com>
2022-01-30 18:30:11 -0300

    refactor: alter name properties numeroTelefone to telefoneCelular.

--- src/jogadores/interfaces/jogador.interface.ts
+++ src/jogadores/interfaces/jogador.interface.ts
@@ -1,6 +1,6 @@
 import { Document } from 'mongoose';
 export interface Jogador extends Document {
-  readonly numeroTelefone: string;
+  readonly telefoneCelular: string;
   readonly email: string;
   nome: string;
   ranking: string;
