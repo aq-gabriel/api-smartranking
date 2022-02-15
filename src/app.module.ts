3f7a6ee22fa5ac6fd1bdda881e6008c63fff340c
Gabriel Aquino <40728047+aq-gabriel@users.noreply.github.com>
2022-02-15 15:21:22 -0300

    feat: add categoriaModule in app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
       'mongodb+srv://admin:nicolas091020@cluster0.k9nfz.mongodb.net/smartranking?retryWrites=true&w=majority',
     ),
     JogadoresModule,
+    CategoriasModule,
   ],
 })
 export class AppModule {}
